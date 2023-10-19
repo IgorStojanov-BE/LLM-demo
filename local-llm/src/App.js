0af346071e03c296475f6ae5078aba58796bdd0f
Igor Stojanov <igor.stojanov@bearingpoint.com>
2023-10-19 16:04:57 +0200

    modify app js

--- local-llm/src/App.js
+++ local-llm/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Chat from './chat';
 
 function App() {
   return (
@@ -18,6 +19,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <h1>Chat App with GPT-3 Integration</h1>
+      <Chat />
     </div>
   );
 }
